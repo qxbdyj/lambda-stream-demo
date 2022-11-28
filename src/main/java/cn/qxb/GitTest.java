b05988086afacf6859ad3f586fb86bcf6b58d186
qxb <1825218227@qq.com>
2022-11-28 20:34:56 +0800

    six commit

--- src/main/java/cn/qxb/GitTest.java
+++ src/main/java/cn/qxb/GitTest.java
@@ -10,5 +10,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
