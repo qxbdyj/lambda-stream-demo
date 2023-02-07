f20539b4167493513ccde1e51bb05d2c42e33a04
qxb <1825218227@qq.com>
2023-02-07 10:01:56 +0800

    2.7-2-5

--- src/main/java/cn/qxb/GitTest.java
+++ src/main/java/cn/qxb/GitTest.java
@@ -26,5 +26,7 @@ public class GitTest {
         System.out.println("傻逼1");
         System.out.println("傻逼2");
         System.out.println("你好");
+        System.out.println("网购");
+        System.out.println("网购2");
     }
 }
